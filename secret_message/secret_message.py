b0ecc17762d53aa9dce702a1a657ceb9e2056ca2
John <john.diket@gmail.com>
2021-05-27 11:06:22 -0400

    adjusted variable names

--- secret_message/secret_message.py
+++ secret_message/secret_message.py
@@ -1,21 +1,21 @@
-import base64\
+import base64
 
 key ='Code_in_Place_2021'
 
 print('What encrypted messge do you want to send: ')
 message = input()
-enc = []
+encryptArray = []
 for i in range(len(message)):
     key_c = key[i % len(key)]
-    enc.append(chr((ord(message[i]) + ord(key_c)) % 256))
+    encryptArray.append(chr((ord(message[i]) + ord(key_c)) % 256))
 
-encryption = base64.urlsafe_b64encode("".join(enc).encode()).decode()
+encryption = base64.urlsafe_b64encode("".join(encryptArray).encode()).decode()
 print("This is the encrypted message:\n" + encryption)
 
-dec = []
+decryptArray = []
 message = base64.urlsafe_b64decode(encryption).decode()
 for i in range(len(message)):
     key_c = key[i % len(key)]
-    dec.append(chr((256 + ord(message[i]) - ord(key_c)) % 256))
+    decryptArray.append(chr((256 + ord(message[i]) - ord(key_c)) % 256))
 
-print('This is the decryption of your message:\n' + "".join(dec))
+print('This is the decryption of your message:\n' + "".join(decryptArray))

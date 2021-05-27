87f00258fe97ec271fdd4e6e811a73d6cb4ae81e
John <john.diket@gmail.com>
2021-05-27 11:10:32 -0400

    adjusted variable naming scheme

--- secret_message/decrypter.py
+++ secret_message/decrypter.py
@@ -6,10 +6,10 @@ encryption = input()
 print('Please enter the decryption key: ')
 key = input()
 
-dec = []
+decryptArray = []
 message = base64.urlsafe_b64decode(encryption).decode()
 for i in range(len(message)):
     key_c = key[i % len(key)]
-    dec.append(chr((256 + ord(message[i]) - ord(key_c)) % 256))
+    decryptArray.append(chr((256 + ord(message[i]) - ord(key_c)) % 256))
 
-print('This is the decryption of your message:\n' + "".join(dec))
+print('This is the decryption of your message:\n' + "".join(decryptArray))
--- secret_message/encrypter.py
+++ secret_message/encrypter.py
@@ -7,10 +7,10 @@ print('Please enter an encryption key: ')
 key = input()
 
 
-enc = []
+encryptArray = []
 for i in range(len(message)):
     key_c = key[i % len(key)]
-    enc.append(chr((ord(message[i]) + ord(key_c)) % 256))
+    encryptArray.append(chr((ord(message[i]) + ord(key_c)) % 256))
 
-encryption = base64.urlsafe_b64encode("".join(enc).encode()).decode()
+encryption = base64.urlsafe_b64encode("".join(encryptArray).encode()).decode()
 print("This is the encrypted message:\n" + encryption)

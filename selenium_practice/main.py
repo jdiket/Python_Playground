8194deb4828f698e3d52dacfaab9e668ceb15689
John <john.diket@gmail.com>
2021-09-01 10:53:04 -0400

    created id element search with implicit wait command

--- selenium_practice/main.py
+++ selenium_practice/main.py
@@ -2,3 +2,8 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
+
+driver.get("https://www.seleniumeasy.com/test/jquery-download-progress-bar-demo.html")
+driver.implicitly_wait(3)
+found_element = driver.find_element_by_id('downloadButton')
+found_element.click()

365798e2bdda85a105ec547949ffdb42c971bee3
John <john.diket@gmail.com>
2021-09-01 11:23:22 -0400

    added wait function for completion output but not displayed

--- selenium_practice/main.py
+++ selenium_practice/main.py
@@ -1,4 +1,7 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
@@ -7,3 +10,10 @@ driver.get("https://www.seleniumeasy.com/test/jquery-download-progress-bar-demo.
 driver.implicitly_wait(3)
 found_element = driver.find_element_by_id('downloadButton')
 found_element.click()
+
+WebDriverWait(driver, 30).until(
+    EC.text_to_be_present_in_element(
+        (By.CLASS_NAME, 'progress-label'),
+        'Complete!'
+    )
+)
